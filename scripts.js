784156beee4ba8284c204afe9ecf194cbef8fa28
Less <166412593+Less-dev@users.noreply.github.com>
2024-06-29 18:22:40 -0500

    update: menu options

---
+++ scripts.js
@@ -0,0 +1,10 @@
+/*
+document.addEventListener('DOMContentLoaded', function () {
+    const aboutMeLink = document.getElementById('aboutMeLink');
+    aboutMeLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Previene el comportamiento por defecto del enlace
+        window.location.href = 'https://github.com/Less-dev';
+    });
+});
+
+*/
