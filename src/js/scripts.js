05132002f86d9cda1d52e92b1e246341f3527e8f
Less <166412593+Less-dev@users.noreply.github.com>
2024-06-30 17:44:17 -0500

    update: scripts.js add the logic for random url

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,29 @@
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    const aboutMeLink = document.getElementById('aboutMeLink');
-    aboutMeLink.addEventListener('click', function (event) {
-        event.preventDefault(); // Previene el comportamiento por defecto del enlace
-        window.location.href = 'https://github.com/Less-dev';
-    });
-});
-
-*/
+function redirectToRandomUrl() {
+    const urls = [
+        "https://sliding.toys/mystic-square/8-puzzle/daily/",
+        "https://alwaysjudgeabookbyitscover.com/",
+        "https://puginarug.com/",
+        "https://optical.toys/",
+        "https://memory.toys/classic/easy/",
+        "https://checkbox.toys/scale/",
+        "https://longdogechallenge.com/",
+        "https://onesquareminesweeper.com/",
+        "https://thatsthefinger.com/",
+        "https://floatingqrcode.com/",
+        "https://clicking.toys/flip-grid/neat-nine/3-holes/",
+        "https://binarypiano.com/",
+        "https://jacksonpollock.org/",
+        "https://smashthewalls.com/",
+        "https://maze.toys/mazes/mini/daily/",
+        "http://www.staggeringbeauty.com/",
+        "https://burymewithmymoney.com/",
+        "https://sliding.toys/klotski/easy-street/",
+        "https://drawing.garden/",
+        "https://paint.toys/calligram/",
+        "https://maninthedark.com/",
+        "https://www.koalastothemax.com/",
+        "https://hackertyper.com/"
+    ];
+    const randomIndex = Math.floor(Math.random() * urls.length);
+    window.open(urls[randomIndex], '_blank');
+}
